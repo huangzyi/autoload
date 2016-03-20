9db0a49381862c8ab6a7fe5a470d9bfdc5acc617
huangzyi <huangzyi@163.com>
2016-03-20 22:04:29 +0800

    Revert "Revert "autoload""
    This reverts commit 5b898200ca56bdcfe47c79d46f24da0db5f7f7df.

---
+++ Hero/Arms/ARM3.class.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Hero\Arms;
+
+
+class ARM1{
+    protected $attack = "X"; // X不得超过10
+    protected $recycle = 1;
+
+    /* 随机化字符串，可以测试你的实例化对象是否已经静态化 */
+    public static $random = "random";
+
+     public function run() {
+        /*
+        * 返回当前类的实例化对象
+        * 请使用单例模式
+        * @link http://www.phptherightway.com/pages/Design-Patterns.html
+        * @link http://dwtedx.com/itshare_355.html
+        * 关于单例模式的好处，请自行了解
+        */
+         if(!(self::$random instanceof self)){
+            self::$random = new self;
+        }
+		return self::$random;
+	}
+    private function __construct(){
+
+	}
+     static public function  test(){
+        echo "调用方法成功";
+    }
+
+
+    public function __clone(){
+        trigger_error("Clone is not allow!",E_USER_ERROR);
+	}
+}
